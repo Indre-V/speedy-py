bbf3449a8f284a95b4f401c781f4c3e27bf51854
Indre Vilickaite <vilickaite@yahoo.ie>
2024-02-05 20:20:00 +0000

    add: color declarations to run.py

--- run.py
+++ run.py
@@ -5,27 +5,39 @@ from colorama import Fore, Back, Style, init
 from os import system, name  
 import time
 
+# Colorama colors
+
+color_blue = Fore.BLUE+Style.NORMAL
+color_cyan = Fore.CYAN+Style.BRIGHT
+color_yellow = Fore.YELLOW+Style.BRIGHT
+color_red = Fore.RED+Style.BRIGHT
+color_green = Fore.GREEN+Style.BRIGHT
+color_magenta = Fore.MAGENTA+Style.BRIGHT
+
+
 # Initialize colorama, autoreset after each use of Colorama
 init(autoreset=True)
 
 
-GAME_LOGO = """
-███████ ██████  ███████ ███████ ██████  ██    ██       ██████  ██    ██ 
-██      ██   ██ ██      ██      ██   ██  ██  ██        ██   ██  ██  ██  
-███████ ██████  █████   █████   ██   ██   ████   █████ ██████    ████   
-     ██ ██      ██      ██      ██   ██    ██          ██         ██    
-███████ ██      ███████ ███████ ██████     ██          ██         ██       
+GAME_LOGO = color_yellow + """
+        
+    ███████ ██████  ███████ ███████ ██████  ██    ██       ██████  ██    ██ 
+    ██      ██   ██ ██      ██      ██   ██  ██  ██        ██   ██  ██  ██  
+    ███████ ██████  █████   █████   ██   ██   ████   █████ ██████    ████   
+         ██ ██      ██      ██      ██   ██    ██          ██         ██    
+    ███████ ██      ███████ ███████ ██████     ██          ██         ██    
+                                                     
     """
 
 def clear_terminal():
     """
-    Clears the terminal.
+    Clears all data from terminal when called
     """
-    #  for windows
+    # for windows
     if name == 'nt':
         _ = system('cls')
 
-    #  for mac and linux
+    # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
 
@@ -33,7 +45,7 @@ def main_menu():
 
     print(GAME_LOGO)
 
-    print(Fore.LIGHTGREEN_EX + """
+    print(Fore.MAGENTA + """
     1. Start Test
     2. View Instructions
     3. Tips and Tricks
@@ -44,16 +56,22 @@ def main_menu():
 
 def display_menu():
     while True:
-        main_menu()  # prints menu
-        option = input(Fore.LIGHTBLUE_EX
+        main_menu() 
+        option = input(color_green
                             + "Please select an option from 1 to 6 "
                               "to continue: "
                             + Style.RESET_ALL)
         clear_terminal()
         
-        validate_range(option, 1, 6)
-
-        if option == "1":
+        validate_range = ['1', '2', '3', '4', '5', '6']
+        
+        if option not in validate_range:
+            print(color_red + "Invalid choice.")
+            print(color_green + "Please choose options 1 to 6 only.")
+            time.sleep(3)
+            display_menu()
+         
+        elif option == "1":
             start_test()
         elif option == "2":
             view_instructions()
@@ -68,6 +86,13 @@ def display_menu():
             clear_terminal()
             exit()
             
+def main():
+    """
+    Main function of the program. Shows app menu, from where user can start
+    and proceed use all the app functionalities.
+    """
+    display_menu()
 
+main()
                                                                                                                    
  
